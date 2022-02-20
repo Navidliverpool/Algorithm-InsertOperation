34ea2a0d6160e9e3021519f583694b4b80da42c3
navid khodaveisi <navidliverpool@yahoo.com>
2022-02-20 03:28:22 +0000

    Add project files.

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] sampleArray = { 1, 3, 5, 7, 8, 14 };
+
+            foreach(var item in sampleArray)
+            {
+                Console.WriteLine("The original array elements are : {0} \n", item);
+            }
+
+            foreach(var item in Engine(sampleArray))
+            {
+                Console.WriteLine("The original array elements are : {0} \n", item);
+            }
+
+            Console.ReadLine();
+        }
+
+        private static int[] Engine(int[] array)
+        {
+            int[] newArray = new int[7];
+            int insertedNum = 10;
+            int poz = 4;
+            bool pozBool = false;
+            bool insertedNumBool = true;
+            bool jumpToSortPoz = false;
+            bool newArrayIndexIJumper = true;
+
+            for (int i = 0; i <= array.Length - 1; i++)
+            {
+                if (newArrayIndexIJumper == true)
+                {
+                    newArray[i] = array[i];
+                }
+
+                if (i == poz - 1)
+                {
+                    if(insertedNumBool == true)
+                    {
+                        newArray[i] = insertedNum;
+                        insertedNumBool = false;
+                        jumpToSortPoz = true;
+                    }
+
+                    if (jumpToSortPoz == true)
+                    {
+                        newArray[i + 1] = array[poz - 1];
+                        pozBool = true;
+                    }
+                }
+
+                if(pozBool == true)
+                {
+                    poz++;
+                    newArrayIndexIJumper = false;
+                }
+            }
+            return newArray;
+        }
+    }
+}
